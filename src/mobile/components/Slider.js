da4860bdc681af62a06176d23758f55e2004fecb
nalindev <nalinnishant.dev@gmail.com>
2021-07-21 01:46:02 +0530

    icon menu

--- src/mobile/components/Slider.js
+++ src/mobile/components/Slider.js
@@ -10,14 +10,37 @@ import "swiper/components/navigation/navigation.min.css"
 
 import "./slide.css";
 
-import SwiperCore, { Autoplay,Pagination,Navigation } from 'swiper/core';
+import SwiperCore, { Autoplay,Pagination,Navigation,Thumbs } from 'swiper/core';
 
-SwiperCore.use([Autoplay,Pagination,Navigation]);
+SwiperCore.use([Autoplay,Pagination,Navigation, Thumbs]);
 
 
 export default function Slider() {
     return (
     <>
+
+    <Swiper id="icobut" spaceBetween={5} slidesPerView={5}>
+            <SwiperSlide key={`covid-19`} tag="li" style={{listStyle: 'none'}}>
+            <img src="https://img.icons8.com/clouds/100/000000/protection-mask.png"/>
+            </SwiperSlide>
+            <SwiperSlide key={`thumb-`} tag="li" style={{listStyle: 'none'}}>
+            <img src="https://img.icons8.com/clouds/100/000000/caduceus.png"/>
+            </SwiperSlide>
+            <SwiperSlide key={`thumb-`} tag="li" style={{listStyle: 'none'}}>
+            <img src="https://img.icons8.com/clouds/100/000000/pill.png"/>
+            </SwiperSlide>
+            <SwiperSlide key={`thumb-`} tag="li" style={{listStyle: 'none'}}>
+            <img src="https://img.icons8.com/clouds/100/000000/cutting-coupon.png"/>
+            </SwiperSlide>
+            <SwiperSlide key={`thumb-`} tag="li" style={{listStyle: 'none'}}>
+            <img src="https://img.icons8.com/clouds/100/000000/potted-plant.png"/>
+            </SwiperSlide>
+            <SwiperSlide key={`thumb-`} tag="li" style={{listStyle: 'none'}}>
+            <img src="https://img.icons8.com/clouds/100/000000/surgical-scissors.png"/>
+            </SwiperSlide>
+    </Swiper>
+
+    {/* banner slider starts from here */}
     <Swiper spaceBetween={0} centeredSlides={true} autoplay={{
   "delay": 1500,
   "disableOnInteraction": false
