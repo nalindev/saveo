7524ff3cb5a8655d5f0ab4d45da74b60c6a375dc
nalindev <nalinnishant.dev@gmail.com>
2021-07-20 18:05:37 +0530

    mobile detection

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Navbar from './mobile/components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './mobile/pages/Home';
 import Login from './mobile/pages/Login';
+import {isMobile} from 'react-device-detect';
 
 function App() {
-  return (
-    <>
-    <Router>
-      <Navbar />
-      <Switch>
+  if(isMobile) {
+    return (
+      <>
+      <Router>
+        <Navbar />
+        <Switch>
         <Route path='/' exact component={Home}/>
         <Route path='/login' component={Login}/>
       </Switch>
-    </Router>
-    </>
-  );
+      </Router>
+      </>
+    );
+  }else{
+    return <h1>Laptop view is currently not Supported</h1>;
+  }
 }
 
 export default App;
